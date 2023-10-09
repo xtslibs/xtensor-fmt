b5689a02f8efdcb7b639a2b3f5abbb97045c6e27
Rohit Goswami <rog32@hi.is>
2023-10-09 21:15:54 +0000

    MAINT: Add a test for views

--- CppCore/tiny_cli.cpp
+++ CppCore/tiny_cli.cpp
@@ -10,6 +10,7 @@
 #include <random>
 
 #include "xtensor-fmt/xarray.hpp"
+#include "xtensor-fmt/xview.hpp"
 
 int main(int argc, char *argv[]) {
   // Eat warnings, also safer
@@ -17,5 +18,13 @@ int main(int argc, char *argv[]) {
   static_cast<void>(argv);
   xt::xarray<double> x = xt::arange<double>(0, 10, 1);
   fmt::print("x = {}\n", x);
+
+  // Views
+  xt::xarray<double> arr2d = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+  auto view1d = xt::view(arr2d, 1);
+  auto view2d = xt::view(arr2d, xt::range(0, 3), xt::range(0, 3));
+  fmt::print("1D View: {}\n", view1d);
+  fmt::print("2D View: {}\n", view2d);
+
   return EXIT_SUCCESS;
 }
