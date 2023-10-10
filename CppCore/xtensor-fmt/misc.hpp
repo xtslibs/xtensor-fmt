01a6ca55fb5eb143f8a08c4395f0e54196598e85
Rohit Goswami <rog32@hi.is>
2023-10-10 00:01:13 +0000

    BUG: Unambigous call for pxt

--- CppCore/xtensor-fmt/misc.hpp
+++ CppCore/xtensor-fmt/misc.hpp
@@ -21,15 +21,4 @@ void pxt(const xt::xarray<T> sinp, std::string name = "var") {
   }
 }
 
-template <typename T>
-void pxt(const xt::xview<T> sinp, std::string name = "var") {
-  fmt::print("Shape of {}: {}\n", name, fmt::join(sinp.shape(), ", "));
-  fmt::print("Number of dimensions of {}: {}\n", name, sinp.dimension());
-  if (sinp.dimension() == 1 || sinp.dimension() == 2 || sinp.dimension() == 0) {
-    fmt::print("{} is:\n{}\n", name, sinp);
-  } else {
-    fmt::print("Only 1D and 2D arrays are supported for direct print.\n");
-  }
-}
-
 } // namespace xtfmt
